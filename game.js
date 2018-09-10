4e4740c8a9a2886601f281bb8afc600032fbcdf8
ektravel <38017980+ektravel@users.noreply.github.com>
2018-09-10 16:10:55 -0500

    added user input validation

--- game.js
+++ game.js
@@ -23,6 +23,20 @@ $(document).ready(function(){
     //target word array
     var displayWord = [];
 
+    //valiable to hold pressed letter
+    var pressedKey;
+
+    //variable to hold remaining guesses
+    var remainingGuesses = 12;
+
+    //variable to hold wins
+    var wins;
+
+    //variable to hold losses
+    var losses;
+
+    $("#remainingGuesses").text(remainingGuesses);
+
     //function to pick a random word from foods array
     function randomWord(){
         targetWord = foods[Math.floor(Math.random() * foods.length)];
@@ -33,38 +47,68 @@ $(document).ready(function(){
 
     //function to replace the letters of the word with underscores
     function hideWord(){
-        for (var letter = 0; letter < targetWord.length; letter++){    
-            displayWord.push(targetWord[letter]);
-            console.log(displayWord);
-            for (var i=0; i < displayWord.length; i++){
+        // for (var letter = 0; letter < targetWord.length; letter++){    
+            // displayWord.push(targetWord[letter]);
+            // console.log(displayWord);
+            // for (var i=0; i < displayWord.length; i++){
+            //     displayWord[i] = "_ ";
+            // }
+            // var newWord = displayWord.join("");
+            // newWord = newWord.split(",");
+            // $("#random-word").text(newWord);
+
+
+        // }
+
+        displayWord = targetWord.split("");
+        console.log(displayWord);
+        for (var i=0; i < displayWord.length; i++){
                 displayWord[i] = "_ ";
-            }
-            var newWord = displayWord.join("");
-            newWord = newWord.split(",");
-            $("#random-word").text(newWord);
         }
+        displayWord = displayWord.join("").split(","); 
+        $("#random-word").text(displayWord);
+
     };
 
     hideWord();
 
-    
+    //function to update remaining guesses
+    function updateGuessCount(){
+        remainingGuesses--;
+        $("#remainingGuesses").text(remainingGuesses);
+    }
+
+
+    $("body").keypress(function(event){
+        console.log(event.key);
+        pressedKey = event.key;
+
+        //user input validation (must be lowecase letter)
+        if (event.which < 97 || event.which > 122){
+            alert("Please use only lowercase letters.");
+            return false;
+        }
+
+        //check if the user guessed correctly
+        //incorrect guess
+        if (targetWord.indexOf(pressedKey) === -1){
+            alert("This letter is not in the word!");
+            updateGuessCount();
+        //correct guess
+        }else{
+            alert("That is correct");
+            var guessedLetter = targetWord.indexOf(pressedKey);
+            $("#random-word").text(displayWord[guessedLetter]);
+        }
+    })
 
 
 
 });
 
 
-
-//TODO: keep track of guesses
-
-//listen for when they press a key that is a letter
-//check to make sure it is a letter
-
-//is this letter in the word that we randomly guessed?
 //yes, update the displayWord to have the guessed letter in the right slot
-//no, decrement their remaining tries
 
-//update the display on the screen 
 
 
 //did they win?

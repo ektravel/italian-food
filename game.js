6fadddbdad902302fd66729b3634bc9e224e83a3
ektravel <38017980+ektravel@users.noreply.github.com>
2018-09-11 21:24:33 -0500

    added styling

--- game.js
+++ game.js
@@ -33,15 +33,13 @@ $(document).ready(function(){
     var wins = 0;
 
     //variable to hold losses
-    var losses;
+    var losses = 0;
 
     $("#remainingGuesses").text(remainingGuesses);
 
     //function to pick a random word from foods array
     function pickRandomWord(){
         targetWord = foods[Math.floor(Math.random() * foods.length)];
-        console.log(targetWord);
-
         targetWord = targetWord.split("");
     };
 
@@ -58,8 +56,6 @@ $(document).ready(function(){
                 displayWord[i] = "_ ";
         }
         updateTargetWord();
-        // $("#random-word").text(displayWord.join(''));
-        // $("#random-word").text(displayWord.join('').split(','));
     };
 
     hideWord();
@@ -71,7 +67,6 @@ $(document).ready(function(){
     };
 
     $("body").keypress(function(event){
-        console.log(event.key);
         pressedKey = event.key;
 
         //user input validation (must be lowecase letter)
@@ -93,31 +88,29 @@ $(document).ready(function(){
         else
             updateGuessCount();
 
+        var messageToDisplay;
+
         //check if the user won the game and update wins
         if (displayWord.indexOf("_ ") === -1){
             wins++;
             $("#wins").text(wins);
-            setTimeout (function() {
-                alert("Congratulations! You won!");
-            }, 300);
+            messageToDisplay = "Congratulations! You won!";
         }
 
         //check if the user lost the game and update losses
-        if (remainingGuesses === 0){
-            alert("You lost!");
+        else if (remainingGuesses === 0){
             losses++;
+            $("#losses").text(losses);
+            messageToDisplay = "You lost!";
         }
 
-
+        //restart
+        if(messageToDisplay){
+            setTimeout (function() {
+                alert(messageToDisplay);
+                pickRandomWord();
+                hideWord();
+            }, 300);
+        }
     })
 });
-
-
-
-
-
-//did they win?
-//yes, then show that they are a winner
-//else if, did they lose?
-//then show they lost
-//else, the game continues
